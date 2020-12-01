369fcc3f8ccd92615cade8eb130d288711c352f0
Immo Heikkinen <immo.heikkinen@zalando.fi>
2020-12-01 08:20:52 +0200

    Day 1, part 2

--- src/day01.clj
+++ src/day01.clj
@@ -11,3 +11,12 @@
             (when (pos? (get (update freqs n dec) (- 2020 n) 0))
               (* n (- 2020 n))))
           freqs)))
+
+(defn part2 [input]
+  (let [freqs (frequencies (parse-input input))]
+    (some (fn [[n1 _]]
+            (some (fn [[n2 _]]
+                    (when (pos? (get (update freqs n2 dec) (- 2020 n1 n2) 0))
+                      (* n1 n2 (- 2020 n1 n2))))
+                  (update freqs n1 dec)))
+          freqs)))
